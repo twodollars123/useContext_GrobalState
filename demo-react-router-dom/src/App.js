59068d00f7acebf9860b8af8f17ca2f13aa70b3d
twodollars123 <twodollars.no2002@gmail.com>
2022-11-07 21:34:02 +0700

    first commit

---
+++ demo-react-router-dom/src/App.js
@@ -0,0 +1,23 @@
+import { Link, Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Bookkeeper!</h1>
+        <nav
+          style={{
+            borderBottom: "solid 1px",
+            paddingBottom: "1rem",
+          }}
+        >
+          <Link to="/invoices">Invoices</Link> |{" "}
+          <Link to="/expenses">Expenses</Link>
+        </nav>
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+export default App;
